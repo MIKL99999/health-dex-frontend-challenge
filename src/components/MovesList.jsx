e314c76aafe5d1f177eb9cb220ab939c6d816bc9
Mikhail.Radziukevich <radykevich@gmail.com>
2019-05-25 19:03:30 +0300

    Add Moves list. Add evolutionChains.

---
+++ src/components/MovesList.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MovesList extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+MovesList.propTypes = {};
--- src/components/PokemonPage.jsx
+++ src/components/PokemonPage.jsx
@@ -1,7 +1,9 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import pokeapi from '../fetch/pokeapi';
+import capitalizeFirstLetter from '../utils/capitalizeFirstLetter';
 
 
 class PokemonPage extends PureComponent {
@@ -11,22 +13,73 @@ class PokemonPage extends PureComponent {
         history: PropTypes.object.isRequired
     };
 
-    async componentDidMount() {
-        // TODO id
-        const res = await pokeapi.getEvolutionChainsList(this.props.match.params.pokemon);
+    state = {
+        movesList: [],
+        evolutionChain: null,
+    };
 
-        console.log(res);
+    async componentDidMount() {
+        const {pokemons, match} = this.props;
+
+        const pokemonName = match.params.pokemon;
+        const pokemon = pokemons.get(pokemonName);
+        const movesList = pokemon.info.moves;
+        this.setState({movesList});
+
+
+        const evolutionChains = await this.getEvolutionChains();
+        const evolutionChain = this.findEvolutionChainByPokemonName(evolutionChains, pokemonName);
+        console.log(evolutionChain);
+
+        this.setState({evolutionChain});
     }
 
+
+    findEvolutionChainByPokemonName = (evolutionChains, pokemonName) => {
+        return evolutionChains.find((evolutionChain) => {
+            const chain = evolutionChain.chain;
+
+            return this.searchPokemonNameInChain(chain, pokemonName);
+        });
+    };
+
+    searchPokemonNameInChain = (chain, pokemonName) => {
+        if (chain.species.name === pokemonName) {
+            return true;
+        }
+
+        return chain.evolves_to.find((c) => this.searchPokemonNameInChain(c, pokemonName));
+    };
+
+    /**
+     * @returns {Array}
+     */
+    getEvolutionChains = async () => {
+        const evolutionChainsUrls = await pokeapi.getEvolutionChainsList();
+
+        const promises = evolutionChainsUrls.results.map(({url}) => {
+            const str = '/evolution-chain/';
+            const index = url.indexOf(str);
+            const id = url.slice(index + str.length, -1);
+
+            return pokeapi.getEvolutionChainById(id);
+        });
+
+        return Promise.all(promises);
+    };
+
     render() {
-        // const {match, location, history} = this.props;
 
         return (
             <div>
-                PokemonInfo
+                <ul>
+                    {this.state.movesList.map(({move}) =>
+                        <li key={move.name}>{capitalizeFirstLetter(move.name)}</li>
+                    )}
+                </ul>
             </div>
         );
     }
 }
 
-export default withRouter(PokemonPage);
+export default withRouter(connect(({pokemons}) => ({pokemons}))(PokemonPage));
