09a72a7bc4ec9c7e284e522355b2c5dbcd3775fe
Mikhail.Radziukevich <radykevich@gmail.com>
2019-05-25 19:02:59 +0300

    Disable redux dev tools in prod.

--- src/redux/config.js
+++ src/redux/config.js
@@ -2,13 +2,12 @@ import {createStore, applyMiddleware, compose} from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from "./reducers";
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
     {},
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        process.env.NODE_ENV !== 'production'? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(): null,
     ),
 );
